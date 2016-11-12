c9f51bdd1264d68b71cc9dea09976d71bb7c937d
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2016-11-12 11:03:35 -0500

    Add exercises for unit testing.

---
+++ unit-test/program.test.js
@@ -0,0 +1,10 @@
+import test from 'tape';
+
+test('Practice the concept of unit testing with simple tape library', nest => {
+    nest.test('Unit test the map function', assert => {
+    assert.equal(actual, expected,
+        `should render default message`);
+
+    assert.end();
+    });
+});
