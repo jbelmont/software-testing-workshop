ba1e24c72331ffc53f9f1f0b7bc8917e032def2e
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2016-11-13 11:32:54 -0500

    Added filter function and unit test.

--- unit-test/exercises/filter/filter.js
+++ unit-test/exercises/filter/filter.js
@@ -1,5 +1,15 @@
-function filter(fn, collection) {
-    
-}
-
-exports.filter = filter;
+/**
+ * Function takes a callback and an array and returns filtered new copy.
+ * @param fn => function
+ * @param collection => Array
+ */
+Array.prototype.customFilter = function(fn) {
+	let results = [];
+    for (let i = 0; i < this.length; i++) {
+        let itemExists = fn(this[i]);
+        if (itemExists) {
+            results.push(this[i]);
+        }
+    }
+	return results;
+};
--- unit-test/solution/solution.js
+++ unit-test/solution/solution.js
@@ -2,13 +2,22 @@ const test = require('tape');
 
 // Load Array.prototype.customMap function
 require('../exercises/map/map');
+require('../exercises/filter/filter');
 
 test('Practice the concept of unit testing with simple tape library', nest => {
 
-    nest.test('Unit test the map function', assert => {
+    nest.test('Unit test the Array.prototype.customMap function', assert => {
         const actual = [1,2,3,4,5];
         const expected = actual.customMap(numbers => numbers);
-        assert.deepEqual(actual, expected, `Should return newer copy of array`);
+        assert.deepEqual(actual, expected, `Should return [1,2,3,4,5]`);
         assert.end();
     });
+
+    nest.test('Unit test the Array.prototype.customFilter function', assert => {
+        const actual = [1,2,3,4,5];
+        const expected = actual.customFilter(num => num > 3);
+        assert.deepEqual([4,5], expected, `Should return [4,5]`);
+        assert.end();
+    });
+
 });
