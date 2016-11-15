08e3deef52e8804cebce58ca04bbf7e6336b96b0
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2016-11-14 19:18:38 -0500

    Added skeleton for integration tests.

---
+++ integration-tests/routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Software Testing' });
+});
+
+module.exports = router;
