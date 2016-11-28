6bae2ce1d1365bb7d840f732c3c482eefb40e5c7
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2016-11-27 19:14:04 -0500

    Added markdown instructions and finalized tdd exercises.

--- tdd/solution/tdd-cycle/cycle1/program.test.js
+++ tdd/solution/tdd-cycle/cycle1/program.test.js
@@ -4,7 +4,7 @@ test('Practice the concept of test-driven development', nest => {
 
     nest.test('Unit test the average function', assert => {
        const tdd = require('./program');
-       assert.throws(tdd.average, {message: 'fn is a function'}, "Average Function Doesn't Exist");
+       assert.ok(tdd.average, "Average Function Doesn't Exist");
        assert.end();  
     });
 
--- tdd/solution/tdd-cycle/cycle2/program.test.js
+++ tdd/solution/tdd-cycle/cycle2/program.test.js
@@ -4,7 +4,7 @@ test('Practice the concept of test-driven development', nest => {
 
     nest.test('Does the average function exist', assert => {
        const tdd = require('./program');
-       assert.doesNotThrow(tdd.average, {message: 'fn is a function'}, "Average Function Does Exist");
+       assert.ok(tdd.average, "Average Function Does Exist");
        assert.end();  
     });
 
