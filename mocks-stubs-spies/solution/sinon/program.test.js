b6363b832294aa22297f74ad5c7848004d74f540
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2016-12-01 18:59:06 -0500

    Added initial skeleton for sinon solution.

---
+++ mocks-stubs-spies/solution/sinon/program.test.js
@@ -0,0 +1,54 @@
+"use strict";
+
+const chai = require('chai');
+const expect = chai.expect;
+const should = chai.should();
+const sinon = require('sinon');
+const request = require('supertest')('http://localhost:3000');
+const statusCodes = require('../../../constants/constants')["statusCodes"];
+
+describe('Practice Using Mocks, Stubs, and Spies using sinon', () => {
+
+    before(() => {
+    
+    });
+
+    after(() => {
+    
+    });
+
+    beforeEach(() => {
+        
+    });
+
+    afterEach(() => {
+
+    });
+
+    describe('Practice Using Mocks in sinon', () => {
+
+        it('', done => {
+            
+            done();  
+        });
+    });
+
+    describe('Practice Using Stubs in sinon', () => {
+
+        it('', done => {
+            
+            done();  
+        });
+
+    });
+
+    describe('Practice Using Spies in sinon', () => {
+
+        it('', done => {
+            
+            done();  
+        });
+
+    });
+
+});
