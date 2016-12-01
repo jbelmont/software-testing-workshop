10e93e299307697ff41d56513b215e131c2cd444
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2016-12-01 18:27:38 -0500

    Added more spies and mocks for crudOperations.

--- mocks-stubs-spies/solution/program.test.js
+++ mocks-stubs-spies/solution/program.test.js
@@ -6,9 +6,16 @@ const nock = require('nock');
 const request = require('supertest')('http://localhost:3000');
 const statusCodes = require('../../constants/constants')["statusCodes"];
 
-let body, getScope, postScope;
+let getScope, postScope, postScope2, deleteScope, deleteScope2, body, body2, body3;
 test('setup', t => {
     const payload = [ 'John J Rambo', 'Conan The Barbarian', 'Billy Jack' ];
+    const requestPostHeaders = {
+        reqheaders: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+        }
+    };
+
     // Mock the configuration request response
     getScope = nock('http://localhost:3000/')
       .get('/api/v1/users/badMofos')
@@ -24,6 +31,24 @@ test('setup', t => {
             {"saying":"3 bucks"},
             {"saying":"4 bucks"},
             {"saying":"5 bucks"}
+        ]
+    };
+
+    const postPayload2 = {
+        "_id": "spicegirls",
+        "_rev": "41-6ac81d7fd42042177fb72ed8de747885",
+        "girls": [
+            "Posh Spice",
+            "Scary Spice",
+            "Baby Spice",
+            "Sporty Spice",
+            "Ginger Spice"
+        ],
+        "songs": [
+            "Wannabe",
+            "Stop",
+            "Viva Forever",
+            "Spice up your life"
         ]
     };
 
@@ -49,9 +74,39 @@ test('setup', t => {
             ]
         }
     };
-    postScope = nock('http://localhost:3000/')
+
+    body2 = {
+        "name": "spicegirls",
+        "document": {
+            "girls": ["Posh Spice", "Scary Spice", "Baby Spice", "Sporty Spice", "Ginger Spice"],
+            "songs": ["Wannabe", "Stop", "Viva Forever", "Spice up your life"]
+        }
+    };
+
+    body3 = {
+	    "name": "spicegirls"
+    };
+
+    postScope = nock('http://localhost:3000/', requestPostHeaders)
         .post('/api/v1/couch/insertDocument/bucks', body)
-        .reply(201, postPayload)
+        .reply(201, postPayload);
+
+    postScope2 = nock('http://localhost:3000/', requestPostHeaders)
+        .post('/api/v1/couch/insertDocument', body2)
+        .reply(201, postPayload2);
+
+    deleteScope = nock('http://localhost:3000/', {
+            'Content-Type': 'application/json'
+        })
+        .post('/api/v1/couch/deleteDocument', body3)
+        .reply(204);
+
+    deleteScope2 = nock('http://localhost:3000/', {
+            'Content-Type': 'application/json'
+        })
+        .post('/api/v1/couch/deleteDocument/spicegirls')
+        .reply(204);
+
     t.end();
 });
 
@@ -62,7 +117,7 @@ test('Practice Testing with Mock', nest => {
             .get('/api/v1/users/badMofos')
             .set('Accept', 'application/json')
             .expect(res => {
-                assert.equal(res.status, ok);
+                assert.equal(res.status, ok, "200 Status Code returned");
                 const soldiers = res.body
                 const expected = [ 'John J Rambo', 'Conan The Barbarian', 'Billy Jack' ];
                 assert.deepEqual(
@@ -72,7 +127,7 @@ test('Practice Testing with Mock', nest => {
                 );
             })
             .end((err, res) => {
-                assert.equal(getScope.isDone(), true, "Get Request was executed with Nock Spy");
+                assert.equal(getScope.isDone(), true, "Get /api/v1/users/badMofos Nock Spy called");
                 assert.end();
             });
     });
@@ -87,17 +142,73 @@ test('Practice Testing with Mock', nest => {
             })
             .send(body)
             .expect(res => {
-                assert.equal(res.status, created);
-                assert.equal(res.body.prices[0], 1);
+                assert.equal(res.status, created, "201 Status Code returned");
+                assert.equal(res.body.prices[0], 1, "Body should contain 1");
             })
             .end((err, res) => {
-                assert.equal(postScope.isDone(), true, "POST Request was executed with Nock Spy");
+                assert.equal(postScope.isDone(), true, "POST /api/v1/couch/insertDocument/:docname Nock Spy called");
+                assert.end();
+            });
+    });
+
+    nest.test('Mock POST request to /api/v1/couch/insertDocument', assert => {
+        const created = statusCodes["create"];
+        request
+            .post('/api/v1/couch/insertDocument')
+            .set({
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            })
+            .send(body2)
+            .expect(res => {
+                assert.equal(res.status, created, "201 Status Code returned");
+                assert.equal(res.body.girls[0], "Posh Spice", "Body should contain Posh Spice as value.");
+            })
+            .end((err, res) => {
+                assert.equal(postScope.isDone(), true, "POST /api/v1/couch/insertDocument Nock Spy called");
+                assert.end();
+            });
+    });
+
+    nest.test('Mock DELETE request to /api/v1/couch/deleteDocument', assert => {
+        const deleted = statusCodes["delete"];
+        request
+            .post('/api/v1/couch/deleteDocument')
+            .set({
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            })
+            .send(body3)
+            .expect(res => {
+                assert.equal(res.status, deleted, "204 Status Code returned");
+            })
+            .end((err, res) => {
+                assert.equal(postScope.isDone(), true, "DELETE /api/v1/couch/deleteDocument Nock Spy called");
+                assert.end();
+            });
+    });
+
+    nest.test('Mock DELETE request to /api/v1/couch/deleteDocument/:docname', assert => {
+        const deleted = statusCodes["delete"];
+        request
+            .post('/api/v1/couch/deleteDocument/spicegirls')
+            .set({
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            })
+            .send(body3)
+            .expect(res => {
+                assert.equal(res.status, deleted, "204 Status Code returned");
+            })
+            .end((err, res) => {
+                assert.equal(postScope.isDone(), true, "DELETE /api/v1/couch/deleteDocument/:docname Nock Spy called");
                 assert.end();
             });
     });
 });
 
 test('teardown', t => {
+    // Cleanup done here.
     nock.cleanAll();
     t.end();
 });
