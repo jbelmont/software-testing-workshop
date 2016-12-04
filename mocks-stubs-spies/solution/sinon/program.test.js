140b6148c33d272a9da3be0cc1f7a1b2c49d6a24
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2016-12-03 23:00:56 -0500

    Add sinon 2.0.0 and assert library for chai.

--- mocks-stubs-spies/solution/sinon/program.test.js
+++ mocks-stubs-spies/solution/sinon/program.test.js
@@ -6,23 +6,34 @@ const expect = chai.expect;
 const should_chai = chai.should();
 const should = require('should');
 const sinon = require('sinon');
+const assert = require('chai').assert;
 
 const DocOperations = require(join(__dirname, '../../../models/crudOperations'));
 const statusCodes = require('../../../constants/constants')["statusCodes"];
 
+const sandbox = sinon.sandbox.create();
 let retrieveDocStub, insertDocStub, deleteDocStub;
 describe('Practice Using Mocks, Stubs, and Spies using sinon', () => {
 
     before(() => {
-        retrieveDocStub = sinon.stub(DocOperations, "retrieveDocument");
-        insertDocStub = sinon.stub(DocOperations, "updateDocument");
-        deleteDocStub = sinon.stub(DocOperations, "deleteDocument");
+        retrieveDocStub = sandbox.stub(DocOperations, "retrieveDocument").returns({
+            "_id": "heman",
+            "_rev": "1-f250259ef8e8c1e6f98734110999246f",
+            "heroSlogan": "I have the power",
+            "He-Man": [
+                "He-Man",
+                "Battle Cat",
+                "Man-At-Arms",
+                "Teela",
+                "Skeletor"
+            ]
+        });
+        insertDocStub = sandbox.stub(DocOperations, "updateDocument");
+        deleteDocStub = sandbox.stub(DocOperations, "deleteDocument");
     });
 
     after(() => {
-        DocOperations.retrieveDocument.restore();
-        DocOperations.updateDocument.restore();
-        DocOperations.deleteDocument.restore();
+        sandbox.restore();
     });
 
     describe('Stub out all the couchDB calls with sinon mocking library.', () => {
@@ -35,22 +46,11 @@ describe('Practice Using Mocks, Stubs, and Spies using sinon', () => {
                 "Teela",
                 "Skeletor"
             ];
-            retrieveDocStub.returns({
-                "_id": "heman",
-                "_rev": "1-f250259ef8e8c1e6f98734110999246f",
-                "heroSlogan": "I have the power",
-                "He-Man": [
-                    "He-Man",
-                    "Battle Cat",
-                    "Man-At-Arms",
-                    "Teela",
-                    "Skeletor"
-                ]
-            });
             let heManDoc = DocOperations.retrieveDocument({ 
                 dbName: 'softwaretesting', 
                 name: 'powerRangers'
             });
+            sinon.assert.calledOnce(retrieveDocStub);
             should.deepEqual(heManDoc["He-Man"], heMan, "He-Man should have his proper foes and heroes.");
             done();
         });
