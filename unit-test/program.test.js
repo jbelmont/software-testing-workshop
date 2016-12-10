afb68be7bd114a4112149c7ca193e77a37060619
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2016-12-10 10:42:09 -0500

    Add required library.

--- unit-test/program.test.js
+++ unit-test/program.test.js
@@ -1,6 +1,15 @@
-import test from 'tape';
+const test = require('tape');
 
-test('Practice the concept of unit testing with simple tape library', nest => {
+// Load Array.prototype.customMap function
+require('../exercises/map/map');
+// Load Array.prototype.customFilter function
+require('../exercises/filter/filter');
+// Load Array.prototype.concatAll function.
+require('../exercises/concatAll/concatAll');
+// Load Array.prototype.concatMap function.
+require('../exercises/concatMap/concatMap');
+
+test('Practice the concept of unit testing with simple tape library', function(nest) {
 
     // TODO: Make unit test pass with actual and expected assertion.
     nest.test('Unit test the map function', assert => {
